ba03121e7b6df7e0926006bca4e0500b476b0383
Rodolfo Fadino <rodolfo@minhavida.com.br>
2014-08-09 08:34:23 -0300

    exemplo de bloqueio

---
+++ DemoIISThreadAsync/DemoIISThreadAsync/Controllers/ExemploController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoIISThreadAsync.Controllers
+{
+    public class ExemploController : Controller
+    {
+        // GET: Exemplo
+        public ActionResult Index()
+        {
+            Thread.Sleep(5000);
+            return View();
+        }
+    }
+}
